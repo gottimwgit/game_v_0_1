c04c22c381de089ce25fba8cda5768c4ce49979f
MM <gottimwgit@gmail.com>
2013-02-13 17:00:01 +0000

    main

---
+++ common.cpp
@@ -0,0 +1,32 @@
+#include "common.h"
+
+SDL_Surface *load_image( string filename )
+{ 
+	SDL_Surface* loadedImage = NULL;
+	SDL_Surface* optimizedImage = NULL; 
+	//Load the image using SDL_image 
+	loadedImage = IMG_Load( filename.c_str() );
+
+	//If the image loaded 
+	if ( loadedImage != NULL ) { 
+		//Create an optimized image 
+		optimizedImage = SDL_DisplayFormat( loadedImage ); 
+		//Free the old image 
+		SDL_FreeSurface( loadedImage );
+	} else {
+		//TODO: error handling
+	}
+
+	return optimizedImage;
+}
+
+void apply_surface( int16_t x, int16_t y,
+			  SDL_Surface *src, SDL_Surface *dest )
+{
+	SDL_Rect offset;
+
+	offset.x = x;
+	offset.y = y;
+	
+	SDL_BlitSurface( src, NULL, dest, &offset );
+}
